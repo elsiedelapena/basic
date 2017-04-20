9db6b5fcd92d1215fb14d0a403c64779eb516fa5
Elsie <elsiedelapena.framgia@gmail.com>
2017-04-20 16:06:05 +0800

    Basic Todo

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Show Task Dashboard
+ */
+
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    //dd(Task::all());
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+    return view('tasks');
+});
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create The Task...
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
+});
+
+
